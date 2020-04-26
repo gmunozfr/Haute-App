822bbf4e818f08ca0dcbfbb4fd9ba8e093dc13f8
Fraginals <gmunozfr@gmail.com>
2020-04-26 02:47:02 +0000

    Finally establishing full connection to MongoDB Atlas.Implementing .gitignore to avoid that .env shows credentials.No displaying password and user details inside index.js

--- server/src/index.js
+++ server/src/index.js
@@ -6,26 +6,23 @@ var app = express()//calling the function required from express
 var port = process.env.PORT || 3000 //defining the port that is going to be used
 var bodyParser = require ('body-parser')// is going to be used by the express server to parse the incoming body of the request
 var mongoose = require('mongoose')//being able to handle the relationship between the models and mongodb server
-
+var cors = require("cors")
+var http = require("http")
 
 var routes = require('./routes')
 var cors =  require('./cors')//it handles the security for cross site scripting
 
-
 mongoose.Promise = global.Promise //this is the global namespace
-mongoose.connect('mongodb://localhost/timepieces', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URL);//connecting to Mongodb Atlas without exposing here the user and password against any attacker 
 mongoose.set('useFindAndModify', false)//to get rid of the warning while compiling
-// 'mongodb+srv://test:ccttestuser@cluster0-0g4sv.mongodb.net/test?retryWrites=true&w=majority'
-// process.env.MONGODB_URL
 mongoose.connection.on('error', (err) => { 
     console.log('Mongodb Error: ', err); 
     process.exit();
 });
 mongoose.connection.on('connected', () => { 
-    console.log('MongoDB is successfully connected');
+    console.log('MongoDB is successfully connected');//establishing connection to MongoDB Atlas
 });
 
-// ('mongodb://localhost/timepieces' , { useNewUrlParser: true})//to avoid warnings I am going to use the "useNewUrlParser"
 
 
 app.use(bodyParser.urlencoded({ extended : true}))
@@ -35,7 +32,7 @@ cors(app)// here the app will be called. Is the cors call where we pass the app 
 routes(app) //passing the app to the routes
 
 
-
 app.listen(port, function(err){
     console.log("The Fabulous Timepieces App is Listening on Port: " + port)
 });
+
